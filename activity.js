e4a7ed47bf22f237e247388e5de3a1bdf4218648
Paulogif-afk <paulogabriellimit@gmail.com>
2025-08-10 22:48:20 +0800

    Add files via upload

---
+++ activity.js
@@ -0,0 +1,24 @@
+var Name = "Paulo Gabriel Limit";
+let Age = "19";
+const PI = 3.1416;
+let isStudent = Number(Age) < 25 ? true : false;
+
+let person = {
+    name: Name,
+    age: Age,
+    address: {
+        street: "159, Santol Street, Purok Anahaw, Buhangin",
+        city: "Davao City",
+        country: "Philippines"
+    },
+    hobbies: ["learn about coding", "playing online games"]
+};
+
+let colors = ["red", "black", "green", "white", "black"];
+
+console.log(Name);
+console.log(Age);
+console.log(PI);
+console.log(isStudent);
+console.log(person);
+console.log(colors);
